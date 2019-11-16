78fb71c977c1d7b9dabeb94d58675a315522ed4f
royaljust <jucolann@microsoft.com>
2019-11-16 16:39:18 -0500

    update build script

--- build-docs.ps1
+++ build-docs.ps1
@@ -1,2 +1,4 @@
-pandoc .\O-UDA-0.1_annotated_discussion-draft.md -o .\O-UDA-0.1_annotated_discussion-draft.docx
-pandoc .\O-UDA-0.1_annotated_discussion-draft.md -o .\O-UDA-0.1_annotated_discussion-draft.pdf
+pandoc .\O-UDA-1.0.md -o .\O-UDA-1.0.docx
+pandoc .\O-UDA-1.0.md -o .\O-UDA-1.0.pdf
+pandoc .\O-UDA-1.0_annotated.md -o .\O-UDA-1.0_annotated.docx
+pandoc .\O-UDA-1.0_annotated.md -o .\O-UDA-1.0_annotated.pdf
